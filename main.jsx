99b2d1727ae88915ea5bf85e161885752b45a414
bellstone <jsjss0218@gmail.com>
2026-06-12 13:43:05 +0900

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SceneFM from "./SceneFM.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SceneFM />
+  </React.StrictMode>
+);
